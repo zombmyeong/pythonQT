66b3498431ad9a5b347067841736b6d5d8e0b4f9
zombie <jmk3610@lycos.co.kr>
2015-07-20 17:55:14 +0900

    Revert "add socket and thread tool"
    This reverts commit 992609ebdab74ee36766edb1b92c99970cf75f6a.

--- main.py
+++ main.py
@@ -1,26 +1,9 @@
 #_*_coding:utf-8_*_
 
 import sys
-import socket
-import tool
-
-s = socket.socket()
-host = socket.gethostname()
-port = 65888
-s.bind((host,port))
-s.liten(10)
-client = []
 
 def main() :
-    while True :
-        c, addr = s.accept()
-        t = tool.clientMangement(c, addr)
-        client.append(t);
-        t.start()
-        print 'connected', addr
-
-    for c in client :
-        del c
+    print "main"
 
 if __name__ == '__main__' :
     main()
